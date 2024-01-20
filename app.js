c972746f5e7b829ba04323ffb65b2b34786470d4
karan <karankumar46226@gmail.com>
2024-01-20 21:54:08 +0530

    mongo url updated

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(methodOverride('_method'));
 
 
 
-const DATABASE_URL=process.env.DATABASE_URL||"mongodb://127.0.0.1:27017";
+const DATABASE_URL=process.env.DATABASE_URL||"mongodb+srv://karan:karan12712@cluster0.z9qdea3.mongodb.net/";
 connectdb(DATABASE_URL)
 
 const transporter = nodemailer.createTransport({
